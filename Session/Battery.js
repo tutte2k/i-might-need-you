b296c7c8d853bef2d229d3e6c7fc26979ffcc127
tutte2k <45877330+tutte2k@users.noreply.github.com>
2023-01-17 09:35:38 +0100

    random battery

--- Session/Battery.js
+++ Session/Battery.js
@@ -2,7 +2,7 @@ export default class Battery {
     constructor({ position }) {
         this.position = position;
         this.path = new Path2D();
-        this.path.arc(500, 75, 50, 0, 2 * Math.PI)
+        this.path.arc(this.position.x, this.position.y, 50, 0, 2 * Math.PI)
         this.markedForDeletion = false;
     }
     draw(ctx) {
--- Session/Session.js
+++ Session/Session.js
@@ -3,33 +3,43 @@ import UI from "./UI.js";
 import Battery from "./Battery.js";
 import Supply from "./Supply.js";
 export default class Session {
-    constructor(canvas) {
-        this.width = canvas.width;
-        this.height = canvas.height;
+  constructor(canvas) {
+    this.width = canvas.width;
+    this.height = canvas.height;
 
-        this.robot = new Robot({ position: { x: this.width / 2, y: this.height / 2 } }, this);
-        this.ui = new UI(this);
-        this.supply = new Supply({ position: { x: this.width / 3, y: this.height / 3 } });
-        this.batteries = []
-        
-        this.batteryTimer = 0;
-        this.batteryInterval = 3000;
+    this.robot = new Robot(
+      { position: { x: this.width / 2, y: this.height / 2 } },
+      this
+    );
+    this.ui = new UI(this);
+    this.supply = new Supply({
+      position: { x: this.width / 3, y: this.height / 3 },
+    });
+    this.batteries = [];
+
+    this.batteryTimer = 0;
+    this.batteryInterval = 3000;
+  }
+
+  update(deltaTime) {
+    const spawnTimerElapsed = this.batteryTimer > this.batteryInterval;
+
+    if (spawnTimerElapsed) {
+      this.addBattery();
+      this.batteryTimer = 0;
+    } else {
+      this.batteryTimer += deltaTime;
     }
-
-    update(deltaTime) {
-        const noBatteries = this.batteries.length === 0;
-
-        const spawnTimerElapsed = this.batteryTimer > this.batteryInterval;
-
-        if (spawnTimerElapsed) {
-            this.addBattery();
-            this.batteryTimer = 0;
-        } else {
-            this.batteryTimer += deltaTime;
-        }
-    }
-    addBattery() {
-        this.batteries.push(new Battery({ position: { x: this.width / 3, y: this.height / 3 } }))
-    }
-
+  }
+  addBattery() {
+    console.log(this.width * Math.random())
+    this.batteries.push(
+      new Battery({
+        position: {
+          x: this.width * Math.random(),
+          y: this.height * Math.random(),
+        },
+      })
+    );
+  }
 }
