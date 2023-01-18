414274f2d6896e038dbfb9cf03c9b9f528f8a5ec
tutte2k <45877330+tutte2k@users.noreply.github.com>
2023-01-18 16:18:04 +0100

    Update Hud.js

--- Game/Hud.js
+++ Game/Hud.js
@@ -5,38 +5,33 @@ export default class Hud {
   draw(ctx) {
     ctx.fillStyle = "black";
     ctx.font = 20 + "px " + "Arial";
-
     ctx.fillText(
-      "Agent power: " +
+      "POWER: " +
         (this.game.agent.power.toFixed(1) > 0
           ? this.game.agent.power.toFixed(1)
           : 0),
       25,
       30
     );
-
     ctx.fillText(
-      "Agent skill: " + this.game.agent.skillLevel.toFixed(1),
+      "SKILL: " + this.game.agent.skillLevel.toFixed(1),
       25,
       60
     );
-
-    ctx.fillText("Agent speed: " + this.game.agent.speed.toFixed(1), 25, 90);
-
     ctx.fillText(
-      "Clicks: " + this.game.clicks.toFixed(0),
+      "CLICKS: " + this.game.clicks.toFixed(0),
       canvas.width * 0.5,
       canvas.height * 0.9 + 30
     );
 
     ctx.fillText(
-      "Supply gain: " + this.game.supply.gain.toFixed(0),
+      "SUPPLY: " + this.game.supply.gain.toFixed(0),
       25,
       canvas.height * 0.9
     );
 
     ctx.fillText(
-      "Max Batteries: " + this.game.maxBatteries.toFixed(0),
+      "MAX BATTERIES: " + this.game.maxBatteries.toFixed(0),
       25,
       canvas.height * 0.9 + 30
     );
